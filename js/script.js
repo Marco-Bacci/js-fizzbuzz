e51c25652d4864f1d9bde5a866271ab05a6dd959
Marco Bacci <marcobacci94@gmail.com>
2025-07-02 17:19:56 +0200

    correction: no variables needed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-//dichiaro le variabili dei multipli di 3, 5 ed entrambi
-let fizz = i % 3 === 0;
-let buzz = i % 5 === 0;
-let fizzBuzz = i % 3 === 0 && i % 3 === 0;
+//dichiaro nomi dei multipli
+//fizz = i % 3 === 0;
+//buzz = i % 5 === 0;
+//fizzBuzz = i % 3 === 0 && i % 3 === 0;
+
 
 for (let i=1; i<=100; i++){
   

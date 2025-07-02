277913a128e6c436e8aba02a206a0340e4a1493a
Marco Bacci <marcobacci94@gmail.com>
2025-07-02 17:58:38 +0200

    second console log showing the number

--- js/script.js
+++ js/script.js
@@ -7,13 +7,16 @@
 for (let i=1; i<=100; i++){
   if (i % 3 === 0 && i % 5 === 0 ){
     console.log("FizzBuzz")
+    console.log(`${i}`)
   }
   
   else if (i % 3 === 0){
     console.log("Fizz")
+    console.log(`${i}`)
   }
   else if (i % 5 === 0){
     console.log("Buzz")
+    console.log(`${i}`)
   }
   
 }
